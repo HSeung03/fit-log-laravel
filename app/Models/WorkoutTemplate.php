0f5411573f154eec74738ed93c1b5c2c7557d5df
HSeung03 <lsh232986@naver.com>
2026-01-12 20:41:11 +0900

    feat: add relationship  function in workout template

--- app/Models/WorkoutTemplate.php
+++ app/Models/WorkoutTemplate.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class WorkoutTemplate extends Model
 {
     //
+    protected $fillable = ['user_id', 'template_name', 'routine_contents'];
+
+    // JSON 데이터를 배열로 자동 변환해주는 설정
+    protected $casts = [
+        'routine_contents' => 'array',
+    ];
+
+    // 이 루틴의 주인(유저)
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
