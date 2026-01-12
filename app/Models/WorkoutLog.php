24a30613ad451d8a30b0f4753cc1aea3369850c1
HSeung03 <lsh232986@naver.com>
2026-01-12 20:41:58 +0900

    feat: add relationship  function in workoutlog

--- app/Models/WorkoutLog.php
+++ app/Models/WorkoutLog.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class WorkoutLog extends Model
 {
     //
+    protected $fillable = ['user_id', 'record_date', 'diet_content', 'workout_results', 'current_weight'];
+
+    protected $casts = [
+        'workout_results' => 'array',
+        'record_date' => 'date',
+    ];
+
+    // 이 일지의 작성자(유저)
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
